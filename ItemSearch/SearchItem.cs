424ef9868cf4c40aa675610a0b4797453da19624
Corentin <88098360+Spiky-73@users.noreply.github.com>
2023-11-27 22:07:09 +0100

    Simplified SetGuideItem

--- ItemSearch/SearchItem.cs
+++ ItemSearch/SearchItem.cs
@@ -170,28 +170,9 @@ public sealed class SearchItem : ILoadable {
             if(!items[1].IsAir) ItemSlot.LeftClick(items, ContextID.GuideItem, 1);
         } else {
             int slot = Guide.Config.guideTile && Guide.IsCraftingTileItem(Main.mouseItem) ? 1 : 0;
-            if (slot == 1) {
-                if(!items[slot].IsAir) {
-                    bool flag = false;
-                    switch (Guide.GetPlaceholderType(item)) {
-                        case PlaceholderType.Condition:
-                        flag = Guide.GetPlaceholderType(Guide.guideTile) == PlaceholderType.Condition && item.Name == Guide.guideTile.Name;
-                        break;
-                        case PlaceholderType.Tile:
-                        flag = item.createTile != -1 && item.createTile == items[slot].createTile;
-                        break;
-                        case PlaceholderType.None:
-                        flag = item.type == items[1].type;
-                        break;
-                    }
-                    if (flag) {
-                        slot = 0;
-                        items[1].TurnToAir();
-                    } else if (items[0].type == item.type) items[0].TurnToAir();
-                } else if (items[0].type == item.type) {
-                    slot = 1;
-                    items[0].TurnToAir();
-                }
+            if (slot == 1 && Guide.GetPlaceholderType(item) == PlaceholderType.None) {
+                if(items[1].type == item.type) items[slot--].TurnToAir();
+                else if (items[0].type == item.type) items[0].TurnToAir();
             }
             ItemSlot.LeftClick(items, ContextID.GuideItem, slot);
         }
