3c2a735492154c2901ae88c749e03d892c68b4d6
Spiky-73 <cocojeanne73@gmail.com>
2024-11-19 13:05:24 +0100

    fixed a conflict between FixedTooltipPosition and TooltipHover

--- ItemActions/FixedTooltipPosition.cs
+++ ItemActions/FixedTooltipPosition.cs
@@ -12,7 +12,7 @@ public class FixedTooltipPosition : ILoadable {
     
     public void Load(Mod mod) {
         On_ItemSlot.Draw_SpriteBatch_ItemArray_int_int_Vector2_Color += HookFindSlotPosition;
-        IL_Main.MouseText_DrawItemTooltip += static il => {
+        IL_Main.MouseTextInner += static il => {
             if (!il.ApplyTo(ILFixTooltip, Configs.ItemActions.FixedTooltipPosition)) Configs.UnloadedItemActions.Value.fixedTooltipPosition = true;
         };
     }
@@ -29,31 +29,20 @@ public class FixedTooltipPosition : ILoadable {
         orig(spriteBatch, inv, context, slot, position, lightColor);
     }
 
-
-    // Does not work because Main.MouseTextCache is private
-    // private static void HookFixTooltip(On_Main.orig_MouseText_DrawItemTooltip orig, Main self, ValueType info, int rare, byte diff, int X, int Y) {
-    //     if (Configs.ItemActions.FixedTooltip && _slotPosition.HasValue) {
-    //         (X, Y) = ((int)_slotPosition.Value.X, (int)_slotPosition.Value.Y);
-    //         _slotPosition = null;
-    //     }
-    //     orig(self, info, rare, diff, X, Y);
-    // }
     private static void ILFixTooltip(ILContext il) {
         ILCursor cursor = new(il);
-
-        cursor.EmitDelegate(() => !TooltipHoverSystem.DrawingFrozenTooltips && Configs.ItemActions.FixedTooltipPosition && _slotPosition.HasValue);
-        ILLabel notFixed = cursor.DefineLabel();
-        cursor.EmitBrfalse(notFixed);
-        cursor.EmitDelegate(() => (int)(_slotPosition!.Value.X + TextureAssets.InventoryBack.Width()*_scale*1.1f));
-        cursor.EmitStarg(4);
-        cursor.EmitDelegate(() => {
-            int y = (int)_slotPosition!.Value.Y;
-            _slotPosition = null;
-            return y;
+        cursor.GotoNext(MoveType.Before, i => i.MatchCall(Reflection.Main.MouseText_DrawItemTooltip));
+        cursor.EmitDelegate((int x, int y) => {
+            if (Configs.ItemActions.FixedTooltipPosition && _slotPosition.HasValue) {
+                x = (int)(_slotPosition.Value.X + TextureAssets.InventoryBack.Width() * _scale * 1.1f);
+                y = (int)_slotPosition.Value.Y;
+            }
+            _ilY = y;
+            return x;
         });
-        cursor.EmitStarg(5);
-        cursor.MarkLabel(notFixed);
+        cursor.EmitDelegate(() => _ilY);
     }
+    private static int _ilY;
 
     private static Vector2? _slotPosition = null;
     private static float _scale;
