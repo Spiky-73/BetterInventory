7093e1e89d730eb1fe5edba00b4d767f085deed5
Spiky-73 <cocojeanne73@gmail.com>
2025-11-23 11:30:38 +0100

    Fixed a bug with BetterRecipeGrid

--- Improvements/BetterRecipeGrid.cs
+++ Improvements/BetterRecipeGrid.cs
@@ -97,7 +97,7 @@ public sealed class BetterRecipeGrid : ILoadable {
         cursor.EmitDelegate(() => {
             if (!Configs.BetterRecipeGrid.NoRecStartOffset) return;
             Main.recStart -= Main.recStart % UILinkPointNavigator.Shortcuts.CRAFT_IconsPerRow;
-            Main.recStart = Math.Min(Main.recStart, SpikysLib.MathHelper.Snap(Main.numAvailableRecipes, UILinkPointNavigator.Shortcuts.CRAFT_IconsPerRow, SpikysLib.MathHelper.SnapMode.Ceiling) - UILinkPointNavigator.Shortcuts.CRAFT_IconsPerRow * UILinkPointNavigator.Shortcuts.CRAFT_IconsPerColumn);
+            Main.recStart = Math.Min(Main.recStart, Math.Max(0, SpikysLib.MathHelper.Snap(Main.numAvailableRecipes, UILinkPointNavigator.Shortcuts.CRAFT_IconsPerRow, SpikysLib.MathHelper.SnapMode.Ceiling) - UILinkPointNavigator.Shortcuts.CRAFT_IconsPerRow * UILinkPointNavigator.Shortcuts.CRAFT_IconsPerColumn));
         });
     }
 
